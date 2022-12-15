04fc432db5380459e0eb9b829f10696a68a5139b
donavance08 <adonisavance@gmail.com>
2022-12-15 12:22:26 +0800

    Minor edit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const sections = document.querySelectorAll("section")
-
+    
     const options = {
         rootMargin: "100px 0px",
         threshold: [0, 0.25, 0.5, 0.75, 1]
@@ -9,9 +9,9 @@ document.addEventListener("DOMContentLoaded", function() {
     let observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
             const navItem = document.getElementById(`nav-${entry.target.id}`)
-
+            
             if (entry.intersectionRatio > 0.51) {
-
+                
                 navItem.classList.add("active")
                 //   entry.target.classList.add('active')
             } else {
